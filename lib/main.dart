8b4c174856d490de6c24ad5207c275f111fe1439
FlutLab.io <>
2022-05-24 11:24:57 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,310 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BelajarAppBar(),
+    );
+  }
+}
+
+class BelajarAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 200.0,
+                floating: false,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text("Belajar SliverAppBar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      )),
+                  background: Image(
+                    image: AssetImage('assets/images/gambar1.jpeg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SliverPersistentHeader(
+                pinned: true,
+                delegate: _SliverAppBarDelegate(
+                  TabBar(
+                    labelColor: Colors.black87,
+                    unselectedLabelColor: Colors.grey,
+                    tabs: [
+                      new Tab(icon: new Icon(Icons.audiotrack), text: "Song"),
+                      new Tab(icon: new Icon(Icons.download), text: "Galery"),
+                    ],
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: TabBarView(children: <Widget>[
+            Song(),
+            Galery(),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class Song extends StatelessWidget {
+  final List lagu = [
+    "IVE - Love Dive",
+    "Glass Animals - Heat Wafes",
+    "Le Sserafim - Fearles",
+    "Sezairi - It's You",
+    "One Direction - Story of My Life",
+    "Tulus - Diri",
+    "Tiara Andini - Janji Setia",
+    "BTS - Butter",
+    "Rizky Febian - Hingga Tua Bersama",
+    "Shawn Mendes - Imagination"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+              child: ListTile(
+            title: Text(lagu[index], style: TextStyle(fontSize: 20)),
+            subtitle: Text("Lagu dari " + lagu[index]),
+            leading: Icon(Icons.audiotrack),
+          ));
+        },
+        itemCount: lagu.length,
+      ),
+    );
+  }
+}
+
+class Galery extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GridView.count(crossAxisCount: 2, children: <Widget>[
+        Container(
+          child: Card(
+            elevation: 10.0,
+            child: Column(children: <Widget>[
+              Image.network(
+                "https://i.ibb.co/DrW4wK8/ive.jpg",
+                height: 150.0,
+                width: 200.0,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 1.0),
+              Text(
+                'IVE - Love Dive',
+                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
+              ),
+            ]),
+          ),
+        ),
+        Container(
+          child: Card(
+            elevation: 10.0,
+            child: Column(children: <Widget>[
+              Image.network(
+                "https://i.ibb.co/MsZGQ4j/Glass-Animals.jpg",
+                height: 150.0,
+                width: 200.0,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 1.0),
+              Text(
+                'Glass Animals - Heat Wafes',
+                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
+              ),
+            ]),
+          ),
+        ),
+        Container(
+          child: Card(
+            elevation: 10.0,
+            child: Column(children: <Widget>[
+              Image.network(
+                "https://i.ibb.co/THC6dXj/Le-Ssrafim.jpg",
+                height: 150.0,
+                width: 200.0,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 1.0),
+              Text(
+                'Le SSerafim - Fearles',
+                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
+              ),
+            ]),
+          ),
+        ),
+        Container(
+          child: Card(
+            elevation: 10.0,
+            child: Column(children: <Widget>[
+              Image.network(
+                "https://i.ibb.co/GWxmTvR/Sezairi.jpg",
+                height: 150.0,
+                width: 200.0,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 1.0),
+              Text(
+                'Sezairi - Its You',
+                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
+              ),
+            ]),
+          ),
+        ),
+        Container(
+          child: Card(
+            elevation: 10.0,
+            child: Column(children: <Widget>[
+              Image.network(
+                "https://i.ibb.co/7XMcyyZ/One-Direction.jpg",
+                height: 150.0,
+                width: 200.0,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 1.0),
+              Text(
+                'One Direction - Story Of  My Life',
+                style: TextStyle(color: Colors.brown, fontSize: 10, fontWeight: FontWeight.bold),
+              ),
+            ]),
+          ),
+        ),
+        Container(
+          child: Card(
+            elevation: 10.0,
+            child: Column(children: <Widget>[
+              Image.network(
+                "https://i.ibb.co/0yvtpwG/Tulus.jpg",
+                height: 150.0,
+                width: 200.0,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 1.0),
+              Text(
+                'Tulus - Diri',
+                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
+              ),
+            ]),
+          ),
+        ),
+        Container(
+          child: Card(
+            elevation: 10.0,
+            child: Column(children: <Widget>[
+              Image.network(
+                "https://i.ibb.co/QHzmPdF/Tiara-Andini.jpg",
+                height: 150.0,
+                width: 200.0,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 1.0),
+              Text(
+                'Tiara Andini - Janji Setia',
+                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
+              ),
+            ]),
+          ),
+        ),
+        Container(
+          child: Card(
+            elevation: 10.0,
+            child: Column(children: <Widget>[
+              Image.network(
+                "https://i.ibb.co/qgr9LFQ/BTS.png",
+                height: 150.0,
+                width: 200.0,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 1.0),
+              Text(
+                'BTS - Butter',
+                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
+              ),
+            ]),
+          ),
+        ),
+        Container(
+          child: Card(
+            elevation: 10.0,
+            child: Column(children: <Widget>[
+              Image.network(
+                "https://i.ibb.co/bzpXymk/Rizky.jpg",
+                height: 150.0,
+                width: 200.0,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 1.0),
+              Text(
+                'Rizky Febian - Hingga Tua Bersama',
+                style: TextStyle(color: Colors.brown, fontSize: 10, fontWeight: FontWeight.bold),
+              ),
+            ]),
+          ),
+        ),
+        Container(
+          child: Card(
+            elevation: 10.0,
+            child: Column(children: <Widget>[
+              Image.network(
+                "https://i.ibb.co/vDjnbRz/Shawn-Mendes.jpg",
+                height: 150.0,
+                width: 200.0,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 1.0),
+              Text(
+                'Shawn Mendes - Imagination',
+                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
+              ),
+            ]),
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new Container(
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
+  }
+}
